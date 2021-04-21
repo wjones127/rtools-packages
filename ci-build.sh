2bf23d0b33769f55114ab126e2ab91397a844e9f
Jeroen Ooms <jeroenooms@gmail.com>
2021-04-21 15:13:58 +0200

    ninja

--- ci-build.sh
+++ ci-build.sh
@@ -38,7 +38,7 @@ pacman --noconfirm -Syyu
 pacman --noconfirm --needed -S git base-devel binutils unzip
 
 # Install core build stuff
-pacman --noconfirm --needed -S mingw-w64-ucrt-x86_64-{crt,winpthreads,gcc,libtre,pkg-config,xz}
+pacman --noconfirm --needed -S mingw-w64-ucrt-x86_64-{winpthreads,gcc,libtre,pkg-config,xz}
 
 # Initiate git
 git_config user.email 'ci@msys2.org'
